9c72c4fca425a4f8885bf72cab0623a89ce47ccb
barfety.alex <barfety.alex@6ad8e0c4-e994-ea97-56b1-316be19df1e1>
2011-04-28 20:21:52 +0000

    First functional request!

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,14 +1,37 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QUrl>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    http = new QHttp(this);
+
+    connect(http, SIGNAL(done(bool)), this, SLOT(updateForm(bool)));
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::on__sendRequest_clicked()
+{
+    QUrl url;
+    url.setPath("/status.xml");
+
+    url.addQueryItem("key", "4b4b65a6071d");
+
+    http->setHost("api.betaseries.com");
+    http->get(url.toString());
+}
+
+void MainWindow::updateForm (bool error) {
+    if (!error) {
+        ui->_result->setText(http->readAll());
+    } else {
+        ui->_result->setText("Fail...");
+    }
 }
