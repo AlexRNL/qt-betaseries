75bd454bb6d968ddd6670a0b0a20eec876957e0d
barfety.alex <barfety.alex@6ad8e0c4-e994-ea97-56b1-316be19df1e1>
2011-05-01 00:53:12 +0000

    Data retreiving in the xml response.

--- trunk/showresult.cpp
+++ trunk/showresult.cpp
@@ -1,27 +1,44 @@
 #include "showresult.h"
 
-ShowResult::ShowResult(QString title, QString url, QObject *parent) :
-        QObject(parent), _title(title), _url(url) {
+ShowResult::ShowResult (QObject *parent) :
+    QObject(parent) {
 
 }
 
-ShowResult::ShowResult(ShowResult const& original) :
-        _title(original.getTitle()), _url(original.getURL()) {
+ShowResult::ShowResult (QString title, QString url, QObject *parent) :
+    QObject(parent), _title(title), _url(url) {
 
 }
 
-ShowResult& ShowResult::operator=(ShowResult const& original) {
+ShowResult::ShowResult (ShowResult const& original) :
+    _title(original.getTitle()), _url(original.getURL()) {
+
+}
+
+ShowResult& ShowResult::operator= (ShowResult const& original) {
     if (this != &original) {
-            _title = original.getTitle();
-            _url = original.getURL();
-        }
+        _title = original.getTitle();
+        _url = original.getURL();
+    }
     return *this;
 }
 
-QString ShowResult::getTitle() const {
+QString ShowResult::getTitle () const {
     return _title;
 }
 
-QString ShowResult::getURL() const {
+QString ShowResult::getURL () const {
     return _url;
+}
+
+void ShowResult::setTitle (QString title) {
+    this->_title = title;
+}
+
+void ShowResult::setURL (QString url) {
+    this->_url = url;
+}
+
+QString ShowResult::toString () const {
+    return getTitle() + " (" + getURL() + ")";
 }
